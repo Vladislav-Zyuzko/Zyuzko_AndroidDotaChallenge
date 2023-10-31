24d204a032ff47d433005486486e2dd551918a45
Vladislav Zyuzko <zyuzko142@gmail.com>
2023-11-01 00:44:08 +0600

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "zyuzko.androiddotachallenge"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "zyuzko.androiddotachallenge"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.coreKtx)
+    implementation(libs.lifecycleKit)
+
+    implementation(platform(libs.composeBom))
+    implementation(libs.composeActivity)
+    implementation(libs.composeUi)
+    implementation(libs.composePreview)
+    implementation(libs.composeMaterial)
+    implementation(libs.composeMaterial3)
+
+
+}
